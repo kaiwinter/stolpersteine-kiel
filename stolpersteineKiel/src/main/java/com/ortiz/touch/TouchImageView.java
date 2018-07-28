4c120c6f3a298d5ea5f8d165b733c0f5e3fd3c8c
Kai Winter <kaiwinter@gmx.de>
2018-07-28 21:01:52 +0200

    Lint-Warnung behoben

--- stolpersteineKiel/src/main/java/com/ortiz/touch/TouchImageView.java
+++ stolpersteineKiel/src/main/java/com/ortiz/touch/TouchImageView.java
@@ -27,6 +27,7 @@ import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
@@ -34,11 +35,10 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.ImageView;
 import android.widget.OverScroller;
 import android.widget.Scroller;
 
-public class TouchImageView extends ImageView {
+public class TouchImageView extends AppCompatImageView {
 
 	private static final String DEBUG = "DEBUG";
 
@@ -1211,14 +1211,8 @@ public class TouchImageView extends ImageView {
 		boolean isPreGingerbread;
 
 		public CompatScroller(Context context) {
-			if (VERSION.SDK_INT < VERSION_CODES.GINGERBREAD) {
-				isPreGingerbread = true;
-				scroller = new Scroller(context);
-
-			} else {
-				isPreGingerbread = false;
-				overScroller = new OverScroller(context);
-			}
+			isPreGingerbread = false;
+			overScroller = new OverScroller(context);
 		}
 
 		public void fling(int startX, int startY, int velocityX, int velocityY, int minX, int maxX, int minY, int maxY) {
