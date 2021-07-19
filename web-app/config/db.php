40d721af67992f4a0f743943dc538e31998b8333
Kai Winter <kaiwinter@gmx.de>
2021-07-19 16:27:01 +0200

    web-app entfernt
    Die wurde verwendet, um die SQLite-DB zu pflegen. Inzwischen wird allerdings Realm verwendet.

--- web-app/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'sqlite:../../stolpersteineKiel\src\main\assets\steine.db',
-    'username' => '',
-    'password' => '',
-    'charset' => 'utf8',
-];
